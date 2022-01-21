e915fdee1863e1dd1bf5cfc2d390c6702623b9ff
tenaciouscri <cridelve@gmail.com>
2022-01-21 14:48:47 +0100

    Black

--- accounts/migrations/0001_initial.py
+++ accounts/migrations/0001_initial.py
@@ -8,32 +8,39 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Account',
+            name="Account",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('password', models.CharField(max_length=128, verbose_name='password')),
-                ('first_name', models.CharField(max_length=50)),
-                ('last_name', models.CharField(max_length=50)),
-                ('username', models.CharField(max_length=50, unique=True)),
-                ('email', models.EmailField(max_length=100, unique=True)),
-                ('phone_number', models.CharField(max_length=50)),
-                ('date_joined', models.DateTimeField(auto_now_add=True)),
-                ('last_login', models.DateTimeField(auto_now_add=True)),
-                ('is_admin', models.BooleanField(default=False)),
-                ('is_staff', models.BooleanField(default=False)),
-                ('is_active', models.BooleanField(default=False)),
-                ('is_superadmin', models.BooleanField(default=False)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("password", models.CharField(max_length=128, verbose_name="password")),
+                ("first_name", models.CharField(max_length=50)),
+                ("last_name", models.CharField(max_length=50)),
+                ("username", models.CharField(max_length=50, unique=True)),
+                ("email", models.EmailField(max_length=100, unique=True)),
+                ("phone_number", models.CharField(max_length=50)),
+                ("date_joined", models.DateTimeField(auto_now_add=True)),
+                ("last_login", models.DateTimeField(auto_now_add=True)),
+                ("is_admin", models.BooleanField(default=False)),
+                ("is_staff", models.BooleanField(default=False)),
+                ("is_active", models.BooleanField(default=False)),
+                ("is_superadmin", models.BooleanField(default=False)),
             ],
             options={
-                'abstract': False,
+                "abstract": False,
             },
             managers=[
-                ('object', django.db.models.manager.Manager()),
+                ("object", django.db.models.manager.Manager()),
             ],
         ),
     ]
--- category/migrations/0001_initial.py
+++ category/migrations/0001_initial.py
@@ -7,22 +7,32 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Category',
+            name="Category",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('category_name', models.CharField(max_length=50, unique=True)),
-                ('slug', models.CharField(max_length=100, unique=True)),
-                ('description', models.TextField(blank=True, max_length=1000)),
-                ('cat_image', models.ImageField(blank=True, upload_to='photos/categories')),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("category_name", models.CharField(max_length=50, unique=True)),
+                ("slug", models.CharField(max_length=100, unique=True)),
+                ("description", models.TextField(blank=True, max_length=1000)),
+                (
+                    "cat_image",
+                    models.ImageField(blank=True, upload_to="photos/categories"),
+                ),
             ],
             options={
-                'verbose_name': 'category',
-                'verbose_name_plural': 'categories',
+                "verbose_name": "category",
+                "verbose_name_plural": "categories",
             },
         ),
     ]
