d564ef534018acbb0a5d6897c67ebc1ef5104849
tenaciouscri <cridelve@gmail.com>
2022-01-21 15:58:28 +0100

    Configured Django media files

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -126,6 +126,11 @@ STATICFILES_DIRS = [
     "ecommerce/static",
 ]
 
+
+# Media files configuration
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR /"media"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
