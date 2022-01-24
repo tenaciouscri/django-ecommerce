ba80c82d4b32dc39e00a388acff8abe9909a8f33
tenaciouscri <cridelve@gmail.com>
2022-01-24 12:27:45 +0100

    Store url, view, template

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -8,4 +8,5 @@ from . import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home, name="home"),
+    path("store/", include("store.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def store(request):
+    return render(request, "store/store.html")
