b56d63682bbb8064f3135d5048e3ee242510cf9a
tenaciouscri <cridelve@gmail.com>
2022-01-24 13:06:19 +0100

    Context processors displaying categories on navbar

--- category/models.py
+++ category/models.py
@@ -1,6 +1,5 @@
-from distutils.command.upload import upload
-from enum import unique
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -12,6 +11,9 @@ class Category(models.Model):
     class Meta:
         verbose_name = "category"
         verbose_name_plural = "categories"
+
+    def get_url(self):
+        return reverse("products_by_category", args=[self.slug])
 
     def __str__(self):
         return self.category_name
