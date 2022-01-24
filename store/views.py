82d327a65d7c488224fac5314a2899646225262a
tenaciouscri <cridelve@gmail.com>
2022-01-24 12:55:45 +0100

    Display products by category

--- store/views.py
+++ store/views.py
@@ -2,18 +2,19 @@ from django.shortcuts import get_object_or_404, render
 from .models import Product
 from category.models import Category
 
-def store(request, category_slug = None):
+
+def store(request, category_slug=None):
     category = None
     products = None
-    
+
     if category_slug is not None:
         categories = get_object_or_404(Category, slug=category_slug)
-        products = Product.objects.filter(category=categories, is_available= True)
+        products = Product.objects.filter(category=categories, is_available=True)
         product_count = products.count()
     else:
         products = Product.objects.all().filter(is_available=True)
         product_count = products.count()
-    
+
     context = {
         "products": products,
         "product_count": product_count,
