3ae5fc8673d9d83d6b84df38cb73d6ff833a1f44
tenaciouscri <cridelve@gmail.com>
2022-01-25 09:30:47 +0100

    Cart view for getting cart items, total and qty

--- cart/views.py
+++ cart/views.py
@@ -38,5 +38,19 @@ def add_cart(request, product_id):
     return redirect("cart")
 
 
-def cart(request):
-    return render(request, "store/cart.html")
+def cart(request, total=0, quantity=0, cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            total += cart_item.product.price * cart_item.quantity
+            quantity += cart_item.quantity
+    except ObjectNotExist:
+        pass  # just ignore
+
+    context = {
+        "total": total,
+        "quantity": quantity,
+        "cart_items": cart_items,
+    }
+    return render(request, "store/cart.html", context)
