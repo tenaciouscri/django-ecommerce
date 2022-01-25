d3833b713e2481a6689f59915ac0530c3d82a74f
tenaciouscri <cridelve@gmail.com>
2022-01-25 13:57:11 +0100

    Check if product is already in cart, view cart

--- store/views.py
+++ store/views.py
@@ -1,6 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
 from .models import Product
 from category.models import Category
+from cart.models import CartItem
+from cart.views import _cart_id
 
 
 def store(request, category_slug=None):
@@ -27,9 +30,12 @@ def product_detail(request, category_slug, product_slug):
         single_product = Product.objects.get(
             category__slug=category_slug, slug=product_slug
         )
+        in_cart = CartItem.objects.filter(
+            cart__cart_id=_cart_id(request), product=single_product
+        ).exists()
     except Exception as e:
         raise e
 
-    context = {"single_product": single_product}
+    context = {"single_product": single_product, "in_cart": in_cart}
 
     return render(request, "store/product_detail.html", context)
