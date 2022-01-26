7dba8c55c13ff15ea67a653abc183fe8d15f47fb
tenaciouscri <cridelve@gmail.com>
2022-01-26 12:57:58 +0100

    Fixing product warning and empty cart issue

--- store/views.py
+++ store/views.py
@@ -19,7 +19,7 @@ def store(request, category_slug=None):
         paged_products = paginator.get_page(page)
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by("id")
         paginator = Paginator(products, 3)
         page = request.GET.get("page")
         paged_products = paginator.get_page(page)
