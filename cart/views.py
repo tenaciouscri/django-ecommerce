7d966f807c411085ee6e61a3052d4dc60dade7bc
tenaciouscri <cridelve@gmail.com>
2022-01-27 12:04:47 +0100

    Product variation model

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,5 @@
 from math import prod
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.core.exceptions import ObjectDoesNotExist
 
@@ -14,6 +15,9 @@ def _cart_id(request):
 
 
 def add_cart(request, product_id):
+    color = request.GET["color"]
+    size = request.GET["size"]
+    return HttpResponse(color + " " + size)
     product = Product.objects.get(id=product_id)  # get the product
     try:
         cart = Cart.objects.get(
