af4c1f2876b20ba7503408aa549512235be62084
tenaciouscri <cridelve@gmail.com>
2022-01-27 14:36:11 +0100

    Get instance of variation

--- cart/models.py
+++ cart/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from store.models import Product
+from store.models import Product, Variation
 
 
 class Cart(models.Model):
@@ -13,6 +13,7 @@ class Cart(models.Model):
 
 class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variations = models.ManyToManyField(Variation, blank=True)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
@@ -20,5 +21,5 @@ class CartItem(models.Model):
     def sub_total(self):
         return self.product.price * self.quantity
 
-    def __str__(self):
+    def __unicode__(self):
         return self.product
--- cart/views.py
+++ cart/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.core.exceptions import ObjectDoesNotExist
 
-from store.models import Product
+from store.models import Product, Variation
 from .models import Cart, CartItem
 
 
@@ -14,12 +14,23 @@ def _cart_id(request):
 
 
 def add_cart(request, product_id):
-    if request.method == "POST":
-        color = request.POST["color"]
-        size = request.POST["size"]
-        print(color, size)
-
     product = Product.objects.get(id=product_id)  # get the product
+    product_variation = []
+    if request.method == "POST":
+        for item in request.POST:
+            key = item
+            value = request.POST[key]
+
+        try:
+            variation = Variation.objects.get(
+                product=product,
+                variation_category__iexact=key,
+                variation_value__iexact=value,
+            )
+            product_variation.append(variation)
+        except:
+            pass
+
     try:
         cart = Cart.objects.get(
             cart_id=_cart_id(request)
