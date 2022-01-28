6994604e212a517f662f37a30a0028fa03d112cb
tenaciouscri <cridelve@gmail.com>
2022-01-28 10:46:17 +0100

    Registration preparation: set up urls & design

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from accounts.forms import RegistrationForm
+from .forms import RegistrationForm
+
+
+def register(request):
+    form = RegistrationForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/register.html", context)
+
+
+def login(request):
+    return render(request, "accounts/login.html")
+
+
+def logout(request):
+    return render(request, "accounts/logout.html")
