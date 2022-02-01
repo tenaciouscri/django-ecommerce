53c895c7dfbc82ad7816abf1a422969025616535
tenaciouscri <cridelve@gmail.com>
2022-02-01 11:27:12 +0100

    Encode user pk, send token-based activation link

--- accounts/views.py
+++ accounts/views.py
@@ -6,6 +6,14 @@ from .models import Account
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
 
+# verification email
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+
 
 def register(request):
     if request.method == "POST":
@@ -27,6 +35,23 @@ def register(request):
             )
             user.phone_number = phone_number
             user.save()
+
+            # USER ACTIVATION
+            current_site = get_current_site(request)
+            mail_subject = "Please activate your account"
+            message = render_to_string(
+                "accounts/account_verification_email.html",
+                {
+                    "user": user,
+                    "domain": current_site,
+                    "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                    "token": default_token_generator.make_token(user),
+                },
+            )
+            to_email = email
+            send_email = EmailMessage(email_subject, message, to=[to_email])
+            send_email.send()
+
             messages.success(request, "Registration successful!")
             return redirect("register")
     else:
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -146,3 +146,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: "danger",
 }
+
+# SMTP configuration
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
+EMAIL_USE_TILS = True
