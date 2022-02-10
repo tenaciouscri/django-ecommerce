c7263428a4da9f951f8a45349dc735544e05876b
tenaciouscri <cridelve@gmail.com>
2022-02-10 16:39:59 +0100

    Place Order view

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from cart.models import CartItem
+from .forms import OrderForm
+
+
+def place_order(request):
+    current_user = request.user
+
+    # If the cart items count is less than or equal to 0,
+    # redirect to shop
+    cart_items = CartItem.objects.filter(user=current_user)
+    cart_count = cart_items.count()
+    if cart_count <= 0:
+        return redirect("store")
+
+    if request.method == "POST":
+        form = OrderForm(request.POST)
