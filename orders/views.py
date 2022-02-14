963559d7ac43ef305ba51b2cc11ef0a9b93f66a0
tenaciouscri <cridelve@gmail.com>
2022-02-14 14:35:00 +0100

    Send transaction details to backend

--- orders/views.py
+++ orders/views.py
@@ -2,13 +2,32 @@ from datetime import datetime
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 import datetime
+import json
 
 from cart.models import CartItem
-from .models import Order
+from .models import Order, Payment
 from .forms import OrderForm
 
 
 def payments(request):
+    body = json.loads(request.body)
+    order = Order.objects.get(
+        user=request.user, is_ordered=False, order_number=body["orderID"]
+    )
+
+    # Store transaction details in payment model
+    payment = Payment(
+        user=request.user,
+        payment_id=body["transID"],
+        payment_method=body["payment_method"],
+        amount_paid=order.order_total,
+        status=body["status"],
+    )
+    payment.save()
+
+    order.payment = payment
+    order.is_ordered = True
+    order.save()
     return render(request, "orders/payments.html")
 
 
