73bf8b4ee174bde6564b4c0411ba06ee58cb8391
tenaciouscri <cridelve@gmail.com>
2022-02-16 15:58:02 +0100

    Displaying customer reviews and ratings

--- store/views.py
+++ store/views.py
@@ -55,10 +55,14 @@ def product_detail(request, category_slug, product_slug):
     except OrderProduct.DoesNotExist:
         orderproduct = None
 
+    # Get the reviews
+    reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
+
     context = {
         "single_product": single_product,
         "in_cart": in_cart,
         "orderproduct": orderproduct,
+        "reviews": reviews,
     }
 
     return render(request, "store/product_detail.html", context)
