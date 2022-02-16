2c0576d5542a2a54ad952ed34905898d0f0ea86e
tenaciouscri <cridelve@gmail.com>
2022-02-16 10:38:20 +0100

    Generate invoice upon order completion

--- orders/views.py
+++ orders/views.py
@@ -150,4 +150,28 @@ def place_order(request, total=0, quantity=0):
 
 
 def order_complete(request):
-    return render(request, "orders/order_complete.html")
+    order_number = request.GET.get("order_number")
+    transID = request.GET.get("payment_id")
+
+    try:
+        order = Order.objects.get(order_number=order_number, is_ordered=True)
+        ordered_products = OrderProduct.objects.filter(order_id=order.id)
+
+        subtotal = 0
+        for i in ordered_products:
+            subtotal += i.product_price * i.quantity
+
+        payment = Payment.objects.get(payment_id=transID)
+
+        context = {
+            "order": order,
+            "ordered_products": ordered_products,
+            "order_number": order.order_number,
+            "transID": payment.payment_id,
+            "payment": payment,
+            "subtotal": subtotal,
+        }
+        return render(request, "orders/order_complete.html", context)
+
+    except (Payment.DoesNotExist, Order.DoesNotExist):
+        return redirect("home")
