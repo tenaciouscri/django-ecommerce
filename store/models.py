dd7fb24be0aae34e7dff33aa38c7364539b1a1c3
tenaciouscri <cridelve@gmail.com>
2022-02-16 16:05:06 +0100

    Rating average calculation

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.db.models import Avg
 
 from category.models import Category
 from accounts.models import Account
@@ -22,6 +23,15 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+
+    def averageReview(self):
+        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(
+            average=Avg("rating")
+        )
+        avg = 0
+        if reviews["average"] is not None:
+            avg = float(reviews["average"])
+        return avg
 
 
 class VariationManager(models.Manager):
