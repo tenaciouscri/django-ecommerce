c7abe1601817e4d570488e22b338de5f2fc1bb58
tenaciouscri <cridelve@gmail.com>
2022-02-16 12:36:33 +0100

    Created rating stars

---
+++ store/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+from .models import ReviewRating
+
+
+class ReviewForms(forms.ModelForm):
+    class Meta:
+        model = ReviewRating
+        fields = ["subject", "review", "rating"]
