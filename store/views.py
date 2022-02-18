61460a1812acba3d833babe456ab0d297885fb47
tenaciouscri <cridelve@gmail.com>
2022-02-18 09:17:54 +0100

    Product gallery implementation

--- store/views.py
+++ store/views.py
@@ -3,7 +3,7 @@ from django.core.paginator import Paginator
 from django.db.models import Q
 from django.contrib import messages
 
-from .models import Product, ReviewRating
+from .models import Product, ProductGallery, ReviewRating
 from category.models import Category
 from cart.models import CartItem
 from cart.views import _cart_id
@@ -61,11 +61,15 @@ def product_detail(request, category_slug, product_slug):
     # Get the reviews
     reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
 
+    # Get the product gallery
+    product_gallery = ProductGallery.objects.filter(product_id=single_product.id)
+
     context = {
         "single_product": single_product,
         "in_cart": in_cart,
         "orderproduct": orderproduct,
         "reviews": reviews,
+        "product_gallery": product_gallery,
     }
 
     return render(request, "store/product_detail.html", context)
