3c0d41f4a37db1ab7d0c31b02fb100b9f06c5c64
tenaciouscri <cridelve@gmail.com>
2022-03-18 09:48:03 +0100

    Update

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True, cast=bool)
 
-ALLOWED_HOSTS = ['django-ecommerce-env.eba-ekciehrp.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
